beb43d39824cea8b7fa2bedfcbd0f99285cd5e5c
metzgerenator <metzgerenator@gmail.com>
2018-04-18 14:13:05 -0700

    finished draft of text views

--- OpenMic/BaseViews/UitextFieldWithWhitePlaceHolder.swift
+++ OpenMic/BaseViews/UitextFieldWithWhitePlaceHolder.swift
@@ -13,16 +13,9 @@ class UitextFieldWithWhitePlaceHolder: UITextField {
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
 
-        self.setValue(UIColor.white, forKeyPath: "_placeholderLabel.textColor")
+        self.setValue(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), forKeyPath: "_placeholderLabel.textColor")
 
     }
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
 
 }
