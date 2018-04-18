f0b1e7716b3d1d93f3fa3c913e4a5c547f35f3b2
metzgerenator <metzgerenator@gmail.com>
2018-04-18 13:29:35 -0700

    added subclass for view background with line

---
+++ OpenMic/BaseViews/UIViewExtensions.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewExtensions.swift
+//  OpenMic
+//
+//  Created by Michael Metzger  on 4/18/18.
+//  Copyright © 2018 Mike. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func addBottomBorder() {
+        let bottomLayter = CALayer()
+        bottomLayter.frame = CGRect(x: 0, y: self.frame.height - 1, width: self.frame.width, height: 1)
+        bottomLayter.backgroundColor = UIColor(white: 1.0, alpha: 1.0).cgColor
+        self.layer.addSublayer(bottomLayter)
+        
+    }
+
+}
---
+++ OpenMic/BaseViews/ViewWithBottomLayer.swift
@@ -0,0 +1,23 @@
+//
+//  ViewWithBottomLayer.swift
+//  OpenMic
+//
+//  Created by Michael Metzger  on 4/18/18.
+//  Copyright © 2018 Mike. All rights reserved.
+//
+
+import UIKit
+
+class ViewWithBottomLayer: UIView {
+
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.addBottomBorder()
+        self.backgroundColor = UIColor.clear
+        //fatalError("init(coder:) has not been implemented")
+    }
+
+
+
+}
