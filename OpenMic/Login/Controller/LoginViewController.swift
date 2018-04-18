bc1af88283f9c17381efb39ce2ff1f484e83e5ae
metzgerenator <metzgerenator@gmail.com>
2018-04-18 12:53:52 -0700

    set login base view

---
+++ OpenMic/Login/Controller/LoginViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LoginViewController.swift
+//  OpenMic
+//
+//  Created by Michael Metzger  on 4/18/18.
+//  Copyright © 2018 Mike. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: BaseViewController {
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        self.navigationController?.isNavigationBarHidden = true
+        
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        self.navigationController?.isNavigationBarHidden = false
+    }
+
+   
+
+   
+
+}
