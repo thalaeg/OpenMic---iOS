02ccb34e085263c9361f51e839e1895faa82c816
metzgerenator <metzgerenator@gmail.com>
2018-04-19 09:37:07 -0700

    finished cutom nav with button

---
+++ OpenMic/BaseViews/BaseViewController /BaseViewController.swift
@@ -0,0 +1,48 @@
+//
+//  BaseViewController.swift
+//  OpenMic
+//
+//  Created by Michael Metzger  on 4/18/18.
+//  Copyright © 2018 Mike. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        setupCustomBackbuttonAndTransNav()
+    
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupBackgroundImage(assetName: "backgroundlogin.png")
+
+
+    }
+    
+    
+    private func setupBackgroundImage(assetName: String) {
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImage.image = UIImage(named: assetName)
+        backgroundImage.contentMode = UIViewContentMode.scaleAspectFill
+        self.view.insertSubview(backgroundImage, at: 0)
+    }
+
+    private func setupCustomBackbuttonAndTransNav() {
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.backgroundColor = .clear
+        self.navigationController?.navigationBar.backIndicatorImage = #imageLiteral(resourceName: "backArrow")
+        self.navigationController?.navigationBar.backIndicatorTransitionMaskImage = #imageLiteral(resourceName: "backArrow")
+        self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.plain, target: nil, action: nil)
+        self.navigationController?.navigationBar.tintColor = .white
+        
+        self.navigationController?.navigationBar.isTranslucent = true
+        
+    }
+
+}
