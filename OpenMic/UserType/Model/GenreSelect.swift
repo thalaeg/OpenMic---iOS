ab100ba4759627960360d1e5d990d15410d7fd6f
metzgerenator <metzgerenator@gmail.com>
2018-04-20 10:52:07 -0700

    added picker view

---
+++ OpenMic/UserType/Model/GenreSelect.swift
@@ -0,0 +1,18 @@
+//
+//  GenreSelect.swift
+//  OpenMic
+//
+//  Created by Michael Metzger  on 4/20/18.
+//  Copyright © 2018 Mike. All rights reserved.
+//
+
+import UIKit
+class PickerSelect: NSObject {
+    var picker: UIPickerView
+    
+    init(picker: UIPickerView) {
+        self.picker = picker
+        super.init()
+    }
+    
+}
