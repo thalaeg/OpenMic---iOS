ab85622f69c2314e0d9484f0e94211a0b83aebca
metzgerenator <metzgerenator@gmail.com>
2018-04-20 16:27:44 -0700

    setup favGenre view

---
+++ OpenMic/PickGenres/View/FavGenreViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FavGenreViewController.swift
+//  OpenMic
+//
+//  Created by Michael Metzger  on 4/20/18.
+//  Copyright © 2018 Mike. All rights reserved.
+//
+
+import UIKit
+
+class FavGenreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
