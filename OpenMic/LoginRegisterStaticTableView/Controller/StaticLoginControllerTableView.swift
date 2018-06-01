88ef28ee94b095637151e6cf5f9335c365c6510e
metzgerenator <metzgerenator@gmail.com>
2018-06-01 15:27:04 -0400

    added automatic dimension height

---
+++ OpenMic/LoginRegisterStaticTableView/Controller/StaticLoginControllerTableView.swift
@@ -0,0 +1,48 @@
+//
+//  StaticLoginControllerTableView.swift
+//  OpenMic
+//
+//  Created by Michael Metzger  on 6/1/18.
+//  Copyright © 2018 Mike. All rights reserved.
+//
+
+import UIKit
+
+class StaticLoginControllerTableView: UITableViewController {
+
+    @IBOutlet var datePickerCell: UITableViewCell!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+
+        
+
+     
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if indexPath.row == 3 {
+            datePickerCell.isHidden = !datePickerCell.isHidden
+            
+           self.tableView.reloadRows(at: [IndexPath(item: 4, section: 0)], with: .automatic)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        if indexPath.row == 4 {
+            switch datePickerCell.isHidden {
+            case true:
+                return 0
+            default:
+                return UITableViewAutomaticDimension
+            }
+        } else {
+            return UITableViewAutomaticDimension
+        }
+        
+    }
+
+   
+}
