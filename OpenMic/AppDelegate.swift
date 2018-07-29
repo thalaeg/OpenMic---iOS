2b070ed6107d168d95d526f6e595f7eedbfe486f
metzgerenator <metzgerenator@gmail.com>
2018-07-29 17:59:49 -0400

    changed table view line separators color

--- OpenMic/AppDelegate.swift
+++ OpenMic/AppDelegate.swift
@@ -38,10 +38,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         //MARK: Change initial storyboard
         self.window = UIWindow(frame: UIScreen.main.bounds)
-        let storyboard = UIStoryboard(name: "AddArtist", bundle: nil)
-               let initialViewController = storyboard.instantiateViewController(withIdentifier: "selectArtist")
-//        let storyboard = UIStoryboard(name: "Login", bundle: nil)
-//        let initialViewController = storyboard.instantiateViewController(withIdentifier: "login")
+//        let storyboard = UIStoryboard(name: "AddArtist", bundle: nil)
+//               let initialViewController = storyboard.instantiateViewController(withIdentifier: "selectArtist")
+        let storyboard = UIStoryboard(name: "Login", bundle: nil)
+        let initialViewController = storyboard.instantiateViewController(withIdentifier: "login")
         self.window?.rootViewController = initialViewController
         self.window?.makeKeyAndVisible()
         
