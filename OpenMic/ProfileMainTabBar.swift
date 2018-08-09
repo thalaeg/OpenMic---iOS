4099666b2070d39c302e1bd8dcc0accc60340fbf
metzgerenagtor <metzgerenator@gmail.com>
2018-08-08 22:06:39 -0400

    added basic static layout for Edit Profile

---
+++ OpenMic/ProfileMainTabBar.swift
@@ -0,0 +1,35 @@
+//
+//  ProfileMainTabBar.swift
+//  OpenMic
+//
+//  Created by Mike on 8/7/18.
+//  Copyright © 2018 Mike. All rights reserved.
+//
+
+import UIKit
+
+class ProfileMainTabBar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tabBar.barTintColor = UIColor.white
+      
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
